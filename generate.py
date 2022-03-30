7083d87c3fcdb3feec5c08a9d3af0cc3a185e391
Laddge <dev.laddge@gmail.com>
2022-03-30 01:36:50 +0000

    [feat] add cache buster

--- generate.py
+++ generate.py
@@ -1,7 +1,15 @@
+import subprocess
 import tanuky
 import sass
 
+
+def get_commit_id():
+    return subprocess.run("git rev-parse --short HEAD", shell=True,
+                          capture_output=True, text=True).stdout
+
+
 tnk = tanuky.Tanuky()
+tnk.globals.update(commit_id=get_commit_id())
 tnk.generate()
 
 sass.compile(dirname=("./src/.ignore/scss", "./dist/css"), output_style="compressed")
